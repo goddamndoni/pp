19dada9bf134ae37ed848ffa6b053ce50042a79a
Danabek <raimkhanovd@gmail.com>
2023-03-08 00:24:36 +0600

    update

--- lab6/dir and files/3.py
+++ lab6/dir and files/3.py
@@ -1,10 +1,10 @@
 import os
 def ifExist(path):
-    if os.path.exists(path)==True:
+    if os.path.exists(path):
         for i in os.listdir(path):
             if os.path.isfile(os.path.join(path, i)):
                 print(i)
-    if os.path.exists(path)==False:
+    else:
         print("no such path")
 a = input()
 ifExist(a)        
