a4d2364c9db3d35bf0ef8a328d386777ef4e05be
Danabek <raimkhanovd@gmail.com>
2023-03-08 22:43:40 +0600

    update

--- lab6/dir and files/2.py
+++ lab6/dir and files/2.py
@@ -1,4 +1,6 @@
 import os
+def existense(path):
+    print(os.access(path, os.F_OK))
 def readabilty(path):
     print(os.access(path,os.R_OK))
 def writeability(path):
@@ -6,6 +8,7 @@ def writeability(path):
 def executability(path):
     print(os.access(path,os.X_OK))
 a = input()
+existense(a)
 readabilty(a)
 writeability(a)
 executability(a)
