d241613ebb66e6fdbb57715dc17a91cb36a3786c
Danabek <raimkhanovd@gmail.com>
2023-03-08 22:58:04 +0600

    update

--- lab6/builtin functions/5.py
+++ lab6/builtin functions/5.py
@@ -1,3 +1,5 @@
 a = tuple(input().split())
 print(any(a))
 # tuple is False when tuple is empty, so output will be False, when input = None
+print(all(a))
+# but task is asking us if all ELEMENTS of tuple are true so all would be correct in this case  
