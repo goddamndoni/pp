d7344ea00665e5dcdb397fe09f3bcaeb5b1dc4a5
Danabek <raimkhanovd@gmail.com>
2023-05-03 23:06:43 +0600

    lab11

--- lab11/1.py
+++ lab11/1.py
@@ -1,6 +1,6 @@
 import psycopg2, re
 
-conn= psycopg2.connect(
+conn = psycopg2.connect(
     host='localhost', 
     database='postgres',
     user='postgres',    
@@ -10,11 +10,11 @@ conn= psycopg2.connect(
 cur = conn.cursor()
 
 cur.execute(
-    '''CREATE OR REPLACE FUNCTION search_from_bk(a VARCHAR, b VARCHAR)
-      RETURNS SETOF phonebook 
+    '''CREATE OR REPLACE FUNCTION search_from_book(a VARCHAR, b VARCHAR)
+      RETURNS SETOF book 
    AS
    $$
-      SELECT * FROM phonebook WHERE name = a AND number = b;
+      SELECT * FROM book WHERE name = a AND phone = b;
    $$
    language sql;
    '''
@@ -36,9 +36,8 @@ cur.execute(
  
     Foreach i slice 1 in array users
     LOOP
-       INSERT INTO phonebook (name, number) VALUES (i[1], i[2]);
+       INSERT INTO book (name, phone) VALUES (i[1], i[2]);
     END LOOP;
- 
  
  END
  $$;
@@ -46,40 +45,40 @@ cur.execute(
 )
 
 cur.execute(
-    '''CREATE OR REPLACE PROCEDURE insert_to_phonebook(nm VARCHAR, phon VARCHAR)
+    '''CREATE OR REPLACE PROCEDURE insert_to_book(nm VARCHAR, phon VARCHAR)
        LANGUAGE plpgsql
        AS $$
        DECLARE 
           cnt INTEGER;
        BEGIN
-          SELECT INTO cnt (SELECT count(*) FROM phonebook WHERE name = nm);
+          SELECT INTO cnt (SELECT count(*) FROM book WHERE name = nm);
           IF cnt > 0 THEN
              UPDATE phonebook
-                SET number = phon
+                SET phone = phon
                 WHERE name = nm;
           ELSE
-             INSERT INTO book(name, number) VALUES (nm, phon);
+             INSERT INTO book(name, phone) VALUES (nm, phon);
              END IF;
        END;
        $$;''')
 
-cur.execute("""CREATE OR REPLACE PROCEDURE delete_from_phonebook(nm VARCHAR)
+cur.execute("""CREATE OR REPLACE PROCEDURE delete_from_book(nm VARCHAR)
 LANGUAGE plpgsql
 AS $$
 DECLARE cnt INTEGER;
 BEGIN
-    SELECT into cnt (SELECT count(*) FROM phonebook WHERE name = nm);
+    SELECT into cnt (SELECT count(*) FROM book WHERE name = nm);
 	IF cnt IS NOT NULL THEN
-        DELETE FROM phonebook
+        DELETE FROM book
 		WHERE name=nm;
     END IF;
 END;
 $$;""")
 
-cur.execute("""CREATE OR REPLACE FUNCTION paginating(a integer, b integer)
-RETURNS SETOF phonebook
+cur.execute("""CREATE OR REPLACE FUNCTION paginatingfrom(a integer, b integer)
+RETURNS SETOF book
 AS $$
-   SELECT * FROM phonebook
+   SELECT * FROM book
 	ORDER BY name
 	LIMIT a OFFSET b;
 $$
@@ -87,22 +86,22 @@ language sql;""")
 
 a = input('search\ninsert\ninsertloop\ndelete\npaginating\n')
 if a == 'search':
-    cur.execute("SELECT search_from_bk('John Doe', '87076002321')")
+    cur.execute("SELECT search_from_book('Danabek', '87074830395')")
     result = cur.fetchall()
-    print(result)
+    print(result[0])
 if a == 'insert':
-    cur.execute("CALL insert_to_phonebook('Raimkhanov Danabek','87081834518')")
+    cur.execute("CALL insert_to_book('Nyssanov Kakimbek','87774545505')")
 if a == 'insertloop':
     cur.execute('''CALL insert_list_of_users(ARRAY[
-    ARRAY['Genadi Golovkin', '87076052769'],
-    ARRAY['Philip Lahm', '87079815569'],
-    ARRAY['John Jones', '87074793780']
+    ARRAY['Neymar JR', '87076052769'],
+    ARRAY['Vini JR', '87079815569'],
+    ARRAY['Raphinha', '87074793780']
 ]);''')
 if a == 'delete':
-    cur.execute("CALL delete_from_phonebook('Leo Messi')")
+    cur.execute("CALL delete_from_book ('Raphinha')")
 if a == 'paginating':
     cur.execute(
-        '''SELECT * FROM paginating(6, 2);'''
+        '''SELECT * FROM paginatingfrom(6, 0);'''
     )
     print(cur.fetchall())
 conn.commit()
